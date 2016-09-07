77bd65c1d8c82218f6c7d85bff91f0560f9f765c
Steven <banguijun@sina.com>
2016-09-07 18:46:03 +0800

    数据库取问题
    存在的问题：
    task_submit中的数据库读取存在问题，没有权限

--- task_statistics/task_submit/views.py
+++ task_statistics/task_submit/views.py
@@ -38,15 +38,15 @@ def task_submit_form_page(request):
 @login_required
 def task_info_page(request):
 	context = {}
-	task_info = ts.objects.all().filter(theExaminer == request.user).latest('submitTime')
-    today = timezone.now()
-	if task_info : 
+	task_info = ts(theExaminer = request.user).objects.all().latest('submitTime')
+	today = timezone.now()
+	if task_info :
 		last_update = task_info.submitTime
 		if ((last_update.year == today.year) and (last_update.month == today.month)) :
 			# get and calc task info
 			decimalContext = Context(prec = 4)	# set Decimal precision
 			# yi tong
-			context['yiTongRenWuLiang'] = mt.objects.all().filter(theExaminer = request.user and year == today.year and month == today.month).monthly_task
+			context['yiTongRenWuLiang'] = mt(theExaminer=request.user, year = today.year, month = today.month).objects.monthlyTask
 			context['yiChuAn'] = task_info.yiChuAn
 			context['xyJian'] = task_info.xyJian
 			context['xyLv'] = task_info.xyJian / task_info.yiChuAn
